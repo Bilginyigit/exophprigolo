aca048c36c4c8fbcab5fb8fac80b645d75c4dec1
bilgin yigit <58143975+Bilginyigit@users.noreply.github.com>
2020-04-05 14:38:57 +0200

    interprétation des codes

--- classe/index.php
+++ classe/index.php
@@ -26,18 +26,35 @@
 
         <!-- Début du code à remplacer par votre PHP -->
         <?php
-        function calcule($calcule){
+$ListeApprenants = array(); //Boucles de la liste en db
+foreach ($dbconnexion->query('SELECT nom, prenom, date_naissance from users') as $Apprenant) { // Appeler des données depuis db
+   echo '<tr><td>'.$Apprenant['nom'].'</td><td>'.$Apprenant['prenom'].'</td><td>'.CalculAge($Apprenant['date_naissance']).'</td></tr>'; //Placer les données à leur place dans le tableau
+ };
+
+ function CalculAge($date_naissance) {
+    $dateN = new DateTime($date_naissance); // appeler la date de naissance
+    $Maintenant = new DateTime(date("Y-m-d"));// date d'appel aujourd'hui
+    $interval = $dateN->diff($Maintenant);// Retirez la date de la date de naissance aujourd'hui
+    return $interval->y.' ans';// montrer le deuil de la personne et la boucle
+}
+
+
+        /*function calcule($calcule){
             $age=date_create($calcule)->diff(date_create('today'))->y;
-            return $age; 
+            return $age; */
+
             /*  $annee_util=explode("-", $date_toute);
             $age=date('Y')-$annee_util[0];
-            echo $age . "  ans"; */ 
-        }
+            echo $age . "  ans";} */ 
+        
+
+        /*
          $getutilisateur = $dbconnexion->query('SELECT nom,prenom,date_naissance FROM users' ) ;
          $resulta= $getutilisateur->fetchAll();
          foreach ($resulta as $value){
-            echo '<tr><td>'.$value[0].'</td><td>'.$value[1].'</td><td>'.calcule($value[2]).'  ans' .'</td></tr>';
-       } ?>
+            echo '<tr><td>'.$value[0].'</td><td>'.$value[1].'</td><td>'.calcule($value[2]).'  ans' .'</td></tr>';}
+        */ 
+       ?>
 
         <!-- Fin du code à remplacer par votre PHP -->
 
